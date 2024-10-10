bdaacb9da7b5a14af24c27afb5f63dd9c4bf09f0
lukmec <mastermuko@t-online.de>
2024-10-10 07:19:48 +0200

    prepare for koyeb hosting

--- api/run_local.py
+++
@@ -1,8 +0,0 @@
-from counter_app import counter_app as app
-
-# main driver function
-if __name__ == '__main__':
-
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run()
--- app.py
+++ app.py
@@ -1,36 +1,6 @@
-# app.py
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/', methods=['GET'])
-# def home():
-#     return "Homepage"
-#
-#
-# @app.route('/contact', methods=['GET'])
-# def contact():
-#     return "Contact page"
-#
-
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello from Koyeb'
-#
-# @app.route('/2')
-# def hello_world2():
-#     return 'Hello from Koyeb 22222222'
-#
-#
-# if __name__ == "__main__":
-#     app.run()
-
-# from api.index import app
+#importing "app" --> Flask Container for the application, that is referenced by procfile or vercel.json
 from counter_app import counter_app as app
 
+#for running locally for testing
 if __name__ == "__main__":
     app.run()
