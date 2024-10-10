6016df6636e8855204f638f1c61fbffeb5f7a779
lukmec <mastermuko@t-online.de>
2024-10-10 02:15:22 +0200

    prepare for koyeb hosting

--- app.py
+++ app.py
@@ -14,17 +14,22 @@
 #     return "Contact page"
 #
 
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello from Koyeb'
+#
+# @app.route('/2')
+# def hello_world2():
+#     return 'Hello from Koyeb 22222222'
+#
+#
+# if __name__ == "__main__":
+#     app.run()
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
-@app.route('/2')
-def hello_world2():
-    return 'Hello from Koyeb 22222222'
-
+from api.index import app
 
 if __name__ == "__main__":
     app.run()
