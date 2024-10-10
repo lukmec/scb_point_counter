93364c4a53c33ef34e81871f1e5f5b325a66d03d
lukmec <mastermuko@t-online.de>
2024-10-10 03:03:11 +0200

    prepare for koyeb hosting

--- api/run_local.py
+++ api/run_local.py
@@ -1,8 +1,8 @@
-from counter_app import counter_app
+from counter_app import counter_app as app
 
 # main driver function
 if __name__ == '__main__':
 
     # run() method of Flask class runs the application
     # on the local development server.
-    counter_app.run()
+    app.run()
